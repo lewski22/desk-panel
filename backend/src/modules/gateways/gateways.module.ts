0501fa6ccf750982807f33b89dcd529485a378b3
lewski22 <124936667+lewski22@users.noreply.github.com>
2026-04-19 15:48:18 +0200

    in

--- backend/src/modules/gateways/gateways.module.ts
+++ backend/src/modules/gateways/gateways.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GatewaysController }  from './gateways.controller';
-import { InAppNotificationsService } from '../inapp-notifications/inapp-notifications.service';
 import { GatewaysService }     from './gateways.service';
 import { GatewaySetupService } from './gateway-setup.service';
 import { InstallController }   from './install.controller';
 import { DatabaseModule }      from '../../database/db.module';
+import { InAppNotificationsModule } from '../inapp-notifications/inapp-notifications.module';
 
 @Module({
-  imports:     [DatabaseModule],
+  imports:     [DatabaseModule, InAppNotificationsModule],
   controllers: [GatewaysController, InstallController],
-  providers:   [GatewaysService, GatewaySetupService, InAppNotificationsService],
+  providers:   [GatewaysService, GatewaySetupService],
   exports:     [GatewaysService, GatewaySetupService],
 })
 export class GatewaysModule {}
